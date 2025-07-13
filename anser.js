48be5382108cfd265f62efa4e7b8cc5a9ec29803
sou-rvca <rvca0414@icloud.com>
2025-07-13 17:18:02 +0900

    anser.js

--- anser.js
+++ anser.js
@@ -4,7 +4,57 @@ const isValid = (s) => {
     var a = Array[num];
     let y = s.split('');
     console.log(y);
+
+    for(let j = 0; j < num; j++){
+        if (a[j].equals(")") || a[j].equals("}") || a[j].equals("]")){
+            let open = "";
+            let close = a[j];
+            let error = "";
+            let error1 = "";
+
+            if (close.equals(")")){
+                    open = "(";
+                    error = "{";
+                    error1 = "[";
+            } else if (close.equals("}")) {
+                    open = "{";
+                    error = "(";
+                    error1 = "[";
+            }else if (close.equals("]")) {
+                    open = "[";
+                    error = "(";
+                    error1 = "{";
+            }
+
+            let matched = Boolean(false);
+
+            for (let k = j - 1; k >= 0; k--) {
+                if (a[k].equals("t")) continue; 
+
+                if (a[k].equals(open)) {
+                    a[k] = "t";  
+                    a[j] = "t";  
+                    matched = true;
+                    break;
+                } else if (a[k].equals(")") || a[k].equals("}") || a[k].equals("]")) {
+                    
+                    result = false;
+                    break;
+                }else if (a[k].equals(error) || a[k].equals(error1) ) {
+                    
+                    result = false;
+                    break;
+                }
+            }
+            if (!matched) {
+                    result = false;
+                    break;
+            }
+        }
+        System.out.println(result);
+    }
 };
+
 let s = '()';
 console.log(isValid(s)); // true
 s = '({)}'
